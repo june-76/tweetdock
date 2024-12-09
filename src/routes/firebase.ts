6c09b74c1d47e297332f7b079faa73c537a0a130
June <fromhelianthus@gmail.com>
2024-12-10 01:42:53 +0900

    feat: set up and implement 'create-account' page UI

---
+++ src/routes/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCVWjd24_JdRmKIQLhpPxYEwYveUJrC-f0",
+    authDomain: "tweetdock-48bbf.firebaseapp.com",
+    projectId: "tweetdock-48bbf",
+    storageBucket: "tweetdock-48bbf.firebasestorage.app",
+    messagingSenderId: "718408251093",
+    appId: "1:718408251093:web:bb8f639f122f6cfbdb545c",
+    measurementId: "G-0SD7YL1BXF",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);

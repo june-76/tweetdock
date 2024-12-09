581c9f4c684af6f29bb48691396dbde240016be2
June <fromhelianthus@gmail.com>
2024-12-10 03:21:08 +0900

    feat: implement login functionality using Firebase Auth

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,47 +1,19 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
-import { styled } from "styled-components";
-
-const Wrapper = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 420px;
-    padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-    font-size: 42px;
-`;
-
-const Form = styled.form`
-    margin-top: 50px;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    width: 100%;
-`;
-
-const Input = styled.input`
-    padding: 10px 20px;
-    border-radius: 50px;
-    border: none;
-    width: 100%;
-    font-size: 16px;
-    &[type="submit"] {
-        cursor: pointer;
-        &:hover {
-            opacity: 0.8;
-        }
-    }
-`;
-
-const Error = styled.span`
-    font-weight: 600;
-    color: tomato;
-`;
+import { auth } from "./firebase";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import {
+    Form,
+    Error,
+    Input,
+    Switcher,
+    Title,
+    Wrapper,
+} from "../components/auth-components";
 
 export default function CreateAccount() {
+    const navigate = useNavigate();
     const [isLoading, setLoading] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -59,21 +31,32 @@ export default function CreateAccount() {
             setPassword(value);
         }
     };
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setError("");
+        if (isLoading || name === "" || email === "" || password === "") return;
         try {
-            // create an account
-            // set the name of the user.
-            // redirect to the home page
+            setLoading(true);
+            const credentials = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            await updateProfile(credentials.user, {
+                displayName: name,
+            });
+            navigate("/");
         } catch (e) {
-            // setError
+            if (e instanceof FirebaseError) {
+                setError(e.message);
+            }
         } finally {
             setLoading(false);
         }
     };
     return (
         <Wrapper>
-            <Title>Log into 𝕏</Title>
+            <Title>Join 𝕏</Title>
             <Form onSubmit={onSubmit}>
                 <Input
                     onChange={onChange}
@@ -105,6 +88,9 @@ export default function CreateAccount() {
                 />
             </Form>
             {error !== "" ? <Error>{error}</Error> : null}
+            <Switcher>
+                Already have an account? <Link to="/login">Log in &rarr;</Link>
+            </Switcher>
         </Wrapper>
     );
 }
